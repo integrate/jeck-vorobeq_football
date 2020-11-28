c0f9d80053f476e0c86476f88e286a822acc2219
jeck-vorobeq <m.a.karasev2011@gmale.com>
2020-11-28 10:54:10 +0300

    wererere

--- __main__.py
+++ __main__.py
@@ -7,8 +7,8 @@ pygame.init()
 qwereschet2 = 0
 qwereschet = 0
 pygame.key.set_repeat(10)
-q = pygame.display.set_mode([1000, SCREENY])
-schet = pygame.font.SysFont("comicsansms", 100, 0, 0)
+screen = pygame.display.set_mode([1000, SCREENY])
+schet = pygame.font.SysFont("microsoftnewtailue", 100, 0, 0)
 were = pygame.Rect(100, 277, 50, 400)
 were2 = pygame.Rect(800, 277, 50, 400)
 qw = pygame.Rect(500, 577, 40, 40)
@@ -19,20 +19,20 @@ while 1 == 1:
     ask = pygame.key.get_pressed()
     # верх
     if ask[pygame.K_w] == 1:
-        were.y -= 3
+        were.y -= 4
     # вниз
     if ask[pygame.K_s] == 1:
-        were.y += 3
+        were.y += 4
     if ask[pygame.K_a] == 1:
         were.x -= 3
     if ask[pygame.K_d] == 1:
         were.x += 3
     # верх2
     if ask[pygame.K_UP] == 1:
-        were2.y -= 3
+        were2.y -= 4
     # вниз2
     if ask[pygame.K_DOWN] == 1:
-        were2.y += 3
+        were2.y += 4
     if ask[pygame.K_LEFT] == 1:
         were2.x -= 3
     if ask[pygame.K_RIGHT] == 1:
@@ -56,12 +56,19 @@ while 1 == 1:
             qw.right=were2.left
 
         # движение вниз верх
-
         qw.y += speedy
         if qw.bottom >= SCREENY:
             speedy = -7
         if qw.top <= 0:
             speedy = 7
+
+    #непробиваемая линия
+    if were.right>=500:
+        were.right=498
+    if were2.left<=500:
+        were2.left=502
+
+
 
     # счёт и голы
     if qw.left <= 0 or qw.right >= 1000:
@@ -78,13 +85,15 @@ while 1 == 1:
         charon = False
 
     # рисуем кадр
-    q.fill([0, 0, 0])
+    screen.fill([0, 0, 0])
     # were+= 1www
-    pygame.draw.rect(q, [5, 45,255], were, 0)
-    pygame.draw.rect(q, [255, 58, 0], were2, 0)
-    pygame.draw.rect(q, [255, 0, 0], qw, 2)
+    pygame.draw.rect(screen, [5, 45, 255], were, 0)
+    pygame.draw.rect(screen, [255, 58, 0], were2, 0)
+    pygame.draw.rect(screen, [255, 0, 0], qw, 2)
+    pygame.draw.line(screen,[255,255,255],[500, 0],[500,700],4)
 
-    pygame.draw.circle(q, [255, 255, 255], [qw.centerx, qw.centery], 20)
+    pygame.draw.circle(screen, [255, 255, 255], [qw.centerx, qw.centery], 20)
     schetr = schet.render(str(qwereschet) + ":" + str(qwereschet2), True, [0, 255, 0])
-    q.blit(schetr, [350, 50])
+    schetwid=schetr.get_width()
+    screen.blit(schetr, [500-schetwid/2, 50])
     pygame.display.flip()
